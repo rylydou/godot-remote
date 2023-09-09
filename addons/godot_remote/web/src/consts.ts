5587fc6689f93b022e0a18a8873a899267ebf792
CiberTurtle <rylydou@gmail.com>
2023-09-09 17:27:03 -0500

    Renamed project

--- addons/godot_remote/web/src/consts.ts
+++ addons/godot_remote/web/src/consts.ts
@@ -6,4 +6,4 @@ export const PING_TIME = 3000 // # of milliseconds between each ping.
 export const UNIT_SIZE = 16 // # of pixels in a unit (like rem units in css).
 
 // Storage
-export const SESSION_STORAGE_KEY = 'gwc:session_id' // The key that is used to store the current session_id in session storage.
+export const SESSION_STORAGE_KEY = 'gremote:session_id' // The key that is used to store the current session_id in session storage.
